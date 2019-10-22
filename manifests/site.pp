cc88db7026ce9bc55553f4023a134e9bb244838d
pykettk <kiel.pykett@mobilefun.co.uk>
2019-10-22 15:17:14 +0100

    Update Site Manifest
    + Ensure the README.md file contains the FQDN of the puppet master

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,10 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README.md':
+    ensure => file,
+    content => "Welcome to ${fqdn}\n",
+  }
 }
 node /^web/ {
   include role::app_server
